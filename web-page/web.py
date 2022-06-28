8106a1f042ba28f14a4300894a816404769f939e
Kotorkovsciy <kotorkovsciy@gmail.com>
2022-06-28 23:08:32 +0300

    Correcting web.py

--- web-page/web.py
+++ web-page/web.py
@@ -14,7 +14,8 @@ SQL = Database('../Products.db')
 
 @app.route("/")
 def index():
-    return render_template('index.html', products = run(SQL.read_prod()), all_quantity_products = run(SQL.quantity_prod()), quantity_products = 10, Countdown_products = 1, page=1)
+    return render_template('index.html', products=run(SQL.read_prod()), all_quantity_products=run(SQL.quantity_prod()), quantity_products=10, Countdown_products=1, page=1)
+
 
 @app.route("/<page>")
 def pages(page):
@@ -23,7 +24,7 @@ def pages(page):
         all_quantity_products = run(SQL.quantity_prod())
         if (page < 0) or (page > all_quantity_products/10-1):
             return pageNotFount(404)
-        return render_template('index.html', products = run(SQL.read_prod_more(page)), all_quantity_products = all_quantity_products, quantity_products = page*10+10, Countdown_products = page*10+1, page=page)
+        return render_template('index.html', products=run(SQL.read_prod_more(page)), all_quantity_products=all_quantity_products, quantity_products=page*10+10, Countdown_products=page*10+1, page=page)
     except:
         return pageNotFount(404)
 
