89110c60a08ee881cf38ab190b01f45b2fb3f5d4
Kotorkovsciy <kotorkovsciy@gmail.com>
2022-06-28 13:48:28 +0300

    update start cmd in bot

--- tele-bot/handlers/client.py
+++ tele-bot/handlers/client.py
@@ -4,7 +4,7 @@ from aiogram import Dispatcher, types
 
 
 async def cmd_start(message: types.Message):   
-    await message.answer("Привет")
+    await message.answer(f"Привет, {message.from_user.first_name}! я бот-магазин здоровых напитков, для ознакомления с ассортиментом нажмите на кнопку \"Магазин\"")
     
 def register_handlers_client(dp: Dispatcher):
     dp.register_message_handler(cmd_start, commands="start")
